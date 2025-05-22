615ba0239ce4df4f48371d84e77196256cc94780
Rik177 <mefodi12@gmail.com>
2025-05-23 00:33:11 +0300

    final fix #2

--- script.js
+++ script.js
@@ -21,12 +21,14 @@ const apartments = [
         image: "images/image3.png"
     }
 ];
+
 const info = document.querySelectorAll(".main__list-info");
 const image = document.querySelector(".slider__image");
 const pagination = document.querySelectorAll(".controls__pagination-button");
 const tabs = document.querySelectorAll(".slider__tabs-button");
 const tabsGroup = document.querySelector(".slider__tabs");
 const fields = ["address", "area", "repairTime", "repairCost"];
+
 function setImage(index) { 
     const apartment = apartments[index];
     fields.forEach((field, i) => { 
@@ -42,6 +44,7 @@ setImage(0);
 const prevButton = document.querySelector(".controls__prev-button");
 const nextButton = document.querySelector(".controls__next-button");
 let index = 0;
+
 prevButton.addEventListener("click", () => { 
     if (index <= 0) {
         index = 2;
@@ -51,6 +54,7 @@ prevButton.addEventListener("click", () => {
     setImage(index);
     underlineHandler(tabs[index]);
 });
+
 nextButton.addEventListener("click", () => { 
     if (index >= 2) {
         index = 0;
@@ -60,8 +64,10 @@ nextButton.addEventListener("click", () => {
     setImage(index);
     underlineHandler(tabs[index]);
 });
+
 const underline = document.createElement("span");
 underline.classList.add("slider__tabs-underline");
+
 tabs.forEach((tab, i) => { 
     tab.addEventListener("click", () => { 
         index = i;
@@ -76,22 +82,29 @@ tabs.forEach((tab, i) => {
         });
     }
 });
+
 tabsGroup.appendChild(underline);
+
 pagination.forEach((dot, i) => { 
     dot.addEventListener("click", () => { 
         setImage(i);
         underlineHandler(tabs[index]);
     });
 })
+
 let resizeTimer;
+
 window.addEventListener("resize", () => {
     underline.style.transition = "none";
     underlineHandler(tabs[index]);
+
     clearTimeout(resizeTimer);
+    
     resizeTimer = setTimeout(() => {
         underline.style.transition = "left 0.3s ease";
     }, 200);
 });
+
 function underlineHandler(tab) { 
     const coords = tab.getBoundingClientRect();
     underline.style.top = coords.bottom + 6.68 + "px";
